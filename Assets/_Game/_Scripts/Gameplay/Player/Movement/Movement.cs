46ef623c88c4ca52550269bb4fbc52ebcddf7ef5
Sp0litak <145229434+Sp0litak@users.noreply.github.com>
2026-08-02 19:48:03 +0300

    Added location

---
+++ Assets/_Game/_Scripts/Gameplay/Player/Movement/Movement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.Windows;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _rotationSpeed = 10f;
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private float _jumpHeight = 2f;
+
+    private CharacterController _controller;
+
+    private InputService _input;
+
+    private float _verticalVelocity;
+
+    public void Initialize()
+    {
+        _controller = GetComponent<CharacterController>();
+
+        _input = ServiceLocator.Get<InputService>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 moveInput = _input.Walk;
+
+        Vector3 forward = _cameraTransform.forward;
+        Vector3 right = _cameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 direction = forward * moveInput.y + right * moveInput.x;
+
+        _controller.Move(direction * _moveSpeed * Time.deltaTime);
+
+        if (_controller.isGrounded)
+        {
+            _verticalVelocity = -2f;
+
+            if (_input.Jump)
+                _verticalVelocity = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+        }
+
+        _verticalVelocity += _gravity * Time.deltaTime;
+
+        _controller.Move(Vector3.up * _verticalVelocity * Time.deltaTime);
+    }
+}
--- Assets/_Game/_Scripts/Gameplay/Player/Player.cs
+++ Assets/_Game/_Scripts/Gameplay/Player/Player.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private CameraLook _cameraLook;
-    [SerializeField] private ThirdPersonMovement _thirdPersonMovement;
+    [SerializeField] private Movement _movement;
     [SerializeField] private Interactor _interactor;
     [SerializeField] private MoneyView _moneyView;
     private Money _money;
     public void Initialize()
     {
         _money = ServiceLocator.Get<Money>();
-        _thirdPersonMovement.Initialize();
+        _movement.Initialize();
         _cameraLook.Initialize();
         _interactor.Initialize();
         _moneyView.Initialize(_money);
